fc166a3e438bb50167bf1e2bf0cc5022f7d4a70b
manikandan <rajavetrivelmani@gmail.com>
2024-05-03 11:56:19 +0530

    linux image

--- main.tf
+++ main.tf
@@ -12,23 +12,13 @@ provider "aws" {
   region = var.aws_region
 }
 
-data "aws_ami" "ubuntu" {
+data "aws_ami" "amazon_linux" {
   most_recent = true
-  owners      = ["099720109477"] // Canonical account ID for Ubuntu AMIs
+  owners      = ["amazon"]
 
   filter {
     name   = "name"
-    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-
-  filter {
-    name   = "root-device-type"
-    values = ["ebs"]
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
   }
 }
 

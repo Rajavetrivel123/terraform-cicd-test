164b078b7b6f62ec5df96db2df901b430dbe4f92
manikandan <rajavetrivelmani@gmail.com>
2024-05-03 12:14:29 +0530

    update state file

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform.s3.22-04-24"
-    key            = "tfstate"
+    key            = "jenkins"
     region         = "ap-south-1"
     encrypt        = true
     dynamodb_table = "terraforms"
